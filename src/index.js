92ef19b31a34651b5570e8b290165c39b32a5652
sunairan <15652778863@163.com>
2018-01-24 19:09:46 +0800

    新项目

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Demo from './my/main'
+// import './index.css';
 // import Demo from './App';
 // import Demo from './cssDemo/Demo';
-import Demo from './cssDemo/flexDemo';
+// import Demo from './cssDemo/flexDemo';
 // import Demo from './reactDemo/demo';
 // import Demo from './componentTest/scrollTest';
 import registerServiceWorker from './registerServiceWorker';
